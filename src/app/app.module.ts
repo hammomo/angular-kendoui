41a528753cc6c0cebaab2921a2931ab5f5ad74da
soxmomomo <evamore121@hotmail.com>
2018-05-21 23:34:20 +0800

    add service for data binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { ButtonsModule } from '@progress/kendo-angular-buttons';
@@ -17,7 +17,7 @@ import { AppRoutingModule } from './/app-routing.module';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule,
+    HttpClientModule,
     BrowserAnimationsModule,
     ButtonsModule,
     GridModule,
