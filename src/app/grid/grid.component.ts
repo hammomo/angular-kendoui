d3a5dcc82b3ab7fb09e020adca81c20ee400dc90
soxmomomo <evamore121@hotmail.com>
2018-05-23 22:50:04 +0800

    edit grid component ready

--- src/app/grid/grid.component.ts
+++ src/app/grid/grid.component.ts
@@ -1,9 +1,14 @@
+import { Observable } from 'rxjs/Observable';
 import { Component, OnInit } from '@angular/core';
-import { products } from './products';
-import { Observable } from 'rxjs';
-import { GridDataResult, DataStateChangeEvent } from '@progress/kendo-angular-grid';
-import { State } from '@progress/kendo-data-query';
-import { CategoriesSevice } from '../northwind.service';
+import { Validators, FormBuilder, FormGroup } from '@angular/forms';
+
+import { GridDataResult } from '@progress/kendo-angular-grid';
+import { State, process } from '@progress/kendo-data-query';
+// import { CategoriesSevice } from '../northwind.service';
+import { EditService } from '../edit.service';
+import { Product } from './products';
+
+import { map } from 'rxjs/operators/map';
 
 @Component({
   selector: 'app-grid',
@@ -11,25 +16,83 @@ import { CategoriesSevice } from '../northwind.service';
   styleUrls: ['./grid.component.scss']
 })
 export class GridComponent implements OnInit {
-
-  // public gridData: any[] = products;
   public view: Observable<GridDataResult>;
-  public state: State = {
+  public gridState: State = {
+    sort: [],
     skip: 0,
-    take: 5
+    take: 10
   }
 
-  constructor(private service: CategoriesSevice) {
-    this.view = service;
-    this.service.query(this.state);
-   }
+  public changes: any = {};
+
+  public constructor(private formBuilder: FormBuilder, public editService: EditService) {}
 
   ngOnInit() {
+    this.view = this.editService.pipe(map(data => process(data, this.gridState)));
+    this.editService.read();
   }
 
-  public dataStateChange(state: DataStateChangeEvent): void {
-    this.state = state;
-    this.service.query(state);
+  public onStateChange(state: State) {
+    this.gridState = state;
+    this.editService.read();
   }
 
+  public cellClickHandler({ sender, rowIndex, columnIndex, dataItem, isEdited}) {
+    if (!isEdited) {
+      sender.editCell(rowIndex, columnIndex, this.createFormGroup(dataItem));
+    }
+  }
+
+  public cellCloseHandler(args: any) {
+    const { formGroup, dataItem } = args;
+
+    if(!formGroup.valid) {
+      args.preventDefault();
+    } else if (!formGroup.dirty) {
+      this.editService.assignValues(dataItem, formGroup.value);
+      this.editService.update(dataItem);
+    }
+  }
+
+  public addHandler({ sender }) {
+    sender.addRow(this.createFormGroup(new Product()));
+  }
+
+  public cancelHandler({ sender, rowIndex }) {
+    sender.closeRow(rowIndex);
+  }
+
+  public saveHandler({ sender, formGroup, rowIndex }) {
+    if (formGroup.valid) {
+      this.editService.create(formGroup.value);
+      sender.closeRow(rowIndex);
+    }
+  }
+
+  public removeHandler({ sender, dataItem }) {
+    this.editService.remove(dataItem);
+    sender.cancelCell();
+  }
+
+  public saveChanges(grid: any): void {
+    grid.closeCell();
+    grid.cancelCell();
+    this.editService.saveChanges();
+  }
+
+  public cancelChanges(grid: any): void {
+    grid.cancelCell();
+    this.editService.cancelChanges();
+  }
+
+  public createFormGroup(dataItem: any): FormGroup {
+    return this.formBuilder.group({
+      'ProductID': dataItem.ProductID,
+      'ProductName': [dataItem.ProductName, Validators.required],
+      'UnitProce': dataItem.UnitProce,
+      'UnitsInStock': [dataItem.UnitsInStock, Validators.compose([Validators.required,
+      Validators.pattern('^[0-9]{1,3}')])],
+      'Discontinued': dataItem.Discontinued
+    })
+  }
 }
